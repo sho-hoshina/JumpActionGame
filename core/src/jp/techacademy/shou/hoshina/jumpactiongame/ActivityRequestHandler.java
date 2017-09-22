f0259bac6e7135921ef701cce15d46277f6ae329
sho-hoshina <i.learn.by.one.step@gmail.com>
2017-09-23 08:42:08 +0900

    最初のコミット

---
+++ core/src/jp/techacademy/shou/hoshina/jumpactiongame/ActivityRequestHandler.java
@@ -0,0 +1,9 @@
+package jp.techacademy.shou.hoshina.jumpactiongame;
+
+/**
+ * Created by kuro on 2017/09/18.
+ */
+
+public interface ActivityRequestHandler {
+    public void showAds(boolean show);
+}
---
+++ core/src/jp/techacademy/shou/hoshina/jumpactiongame/JumpActionGame.java
@@ -0,0 +1,24 @@
+package jp.techacademy.shou.hoshina.jumpactiongame;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class JumpActionGame extends Game {
+	//public にして外からアクセスできるようにする
+	public SpriteBatch batch;
+	public ActivityRequestHandler mRequestHandler;
+
+	public JumpActionGame(ActivityRequestHandler requestHandler) {
+		super();
+		mRequestHandler = requestHandler;
+	}
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+
+		// GameScreenを表示する
+		setScreen(new GameScreen(this));
+	}
+
+}
